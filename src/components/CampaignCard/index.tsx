f3780c868aeda1d50a02933592ec6a03f43d4e1b
lucas <lucasalves.souza01@gmail.com>
2024-01-09 10:52:38 -0300

    feat: add author field to campaign card

--- src/components/CampaignCard/index.tsx
+++ src/components/CampaignCard/index.tsx
@@ -38,9 +38,29 @@ const CampaignCard = (campaign: Campaign) => {
                 </div>
 
             </div>
-            <div className="px-6 pt-4 pb-2">
+            <div className="px-6 py-4">
                 <span
                     className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{category}</span>
+
+                <div className="flex mt-3 items-center">
+                    <svg viewBox="0 0 36 36" fill="none" role="img" xmlns="http://www.w3.org/2000/svg" width="25"
+                         height="80">
+                        <mask id=":r7:" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36">
+                            <rect width="36" height="36" rx="72" fill="#FFFFFF"></rect>
+                        </mask>
+                        <g mask="url(#:r7:)">
+                            <rect width="36" height="36" fill="#ff005b"></rect>
+                            <rect x="0" y="0" width="36" height="36"
+                                  transform="translate(0 0) rotate(324 18 18) scale(1)" fill="#ffb238" rx="36"></rect>
+                            <g transform="translate(-4 -4) rotate(-4 18 18)">
+                                <path d="M15 19c2 1 4 1 6 0" stroke="#000000" fill="none" strokeLinecap="round"></path>
+                                <rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect>
+                                <rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect>
+                            </g>
+                        </g>
+                    </svg>
+                    <span className="text-[#808191] text-base text-justify ml-2">by <b>0xC6223A6544A8...</b></span>
+                </div>
             </div>
         </div>
     )
