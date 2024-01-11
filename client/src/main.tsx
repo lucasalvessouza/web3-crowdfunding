15897aff24366b86f422f1e3727d84df0f351657
lucas <lucasalves.souza01@gmail.com>
2024-01-11 18:44:26 -0300

    chore: adding ThirdWeb wrapper to the root

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import { CrowdfundingProvider } from "./context/CrowdfundingContext"
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
 
 createRoot(document.getElementById('root')!).render(
+  <ThirdwebProvider activeChain={Sepolia}>
     <CrowdfundingProvider>
-        <Router>
-            <App />
-        </Router>
+      <Router>
+        <App />
+      </Router>
     </CrowdfundingProvider>,
+  </ThirdwebProvider>
 )
